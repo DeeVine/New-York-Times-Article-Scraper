6a95def6a12eaa506c7b3d4cfe523e162c7a8de0
Darren Kwong <kwongdarren@gmail.com>
2017-12-08 21:59:16 -0800

    added in router

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Main from "./components/Main";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-        </header>
-        <Main />    
-      </div>
-    );
-  }
-}
+
+
+
+const App = () =>
+  <Router>
+    <div> 
+      <Switch>
+        <Route exact path="/" component={Main} />
+      </Switch>
+    </div>
+  </Router>;
 
 export default App;
